079b49c199d27abf72d9ab74a9d92c7b2eb089fb
Jubraj001 <jubrajdev001@gmail.com>
2024-03-31 16:53:23 +0530

    feat: nit

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const options = {
 };
 const swaggerSpec = swaggerJSDoc(options);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', auth);
 app.use('/api/data', data);
 
