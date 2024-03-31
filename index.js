743c4e71e08cf66b1803fd7c8ce18b456c5ff930
Jubraj001 <jubrajdev001@gmail.com>
2024-03-31 17:05:19 +0530

    refactor: revert

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const options = {
 };
 const swaggerSpec = swaggerJSDoc(options);
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', auth);
 app.use('/api/data', data);
 
